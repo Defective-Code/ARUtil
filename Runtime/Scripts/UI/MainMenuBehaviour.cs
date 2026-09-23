dd49f80a006caf8e5637eee76ba9feaad4f794b9
unknown <you@example.com>
2026-09-24 09:29:38 +1200

    Added hud start after main menu is closed

--- Runtime/Scripts/UI/MainMenuBehaviour.cs
+++ Runtime/Scripts/UI/MainMenuBehaviour.cs
@@ -4,7 +4,11 @@ public class MainMenuBehaviour : UIViewBehaviour
 {
     protected override void OnInitialize()
     {
-        Root.Q<Button>("start-button").clicked += () => UIManager.Instance.Pop();
+        Root.Q<Button>("start-button").clicked += () =>
+        {
+            UIManager.Instance.SetHudVisible(true); // set the hud to be visible 
+            UIManager.Instance.Pop();
+        };
         Root.Q<Button>("anchor-menu-button").clicked += () => UIManager.Instance.Push(UIScreen.AnchorMenu);
         Root.Q<Button>("playback-menu-button").clicked += () => UIManager.Instance.Push(UIScreen.PlaybackMenu);
         Root.Q<Button>("map-menu-button").clicked += () => UIManager.Instance.Push(UIScreen.Map);
